e0715fe4259343612a188105e7c76f228c33d5fa
Aman <133687588+Amanmalik5211@users.noreply.github.com>
2026-01-03 10:48:58 +0530

    fix errors

--- node-app/config/database.js
+++ node-app/config/database.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI;
+    const mongoURL = process.env.MONGODB_URL;
     
-    await mongoose.connect(mongoURI);
+    if (!mongoURL) {
+      console.error('MongoDB connection error: MONGODB_URL is not defined in environment variables');
+      console.log('Please create a .env file in the node-app directory with: MONGODB_URL=your_connection_string');
+      process.exit(1);
+    }
+    
+    await mongoose.connect(mongoURL);
     console.log(' MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
-    console.log('Check: 1) Atlas cluster active 2) IP whitelisted 3) Internet connection');
+    console.log('Check: 1) Atlas cluster active 2) IP whitelisted 3) Internet connection 4) MONGODB_URL is correct');
     process.exit(1);
   }
 };
